b559cbe88845d0da95aa55d6c2d22719e65e2862
Elshad Usubov <92145698+Usubov-code@users.noreply.github.com>
2021-12-09 18:19:49 +0400

    First jQuery

---
+++ script.js
@@ -0,0 +1,65 @@
+// $(document).ready(function()
+// {
+// $("p").click(function()
+// {
+//     $(this).toggle();
+// })
+
+// });
+
+// $(document).ready(function()
+// {
+
+//  $('#flip').click(function()
+//  {
+//      $('#panel').slideToggle("slow");
+//  })
+
+
+
+
+// })
+
+
+// $(document).ready(function()
+// {
+
+//     $('button').click(function(){
+//         $('div').animate({left:'toggle'})
+//     })
+
+
+
+
+
+
+// }
+
+// )
+
+// $(document).ready(function()
+// {
+
+//  $('#flip').click(function()
+//  {
+//      $('#panel').slideDown(5000);
+//  });
+//  $('#stop').click(function()
+//  {
+//      $('#panel').stop();
+//  })
+
+
+
+
+// })
+
+$(document).ready(function()
+{
+
+$('button').click(function(){
+    $('#div1').width(500).height(500);
+})
+
+
+})
